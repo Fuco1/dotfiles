4bfa41f92d046c16b36743901457337caf3a7d51
Matus Goljer <matus.goljer@gmail.com>
2015-10-04 18:36:18 +0200

    [xmonad] Add mpris listener for xmobar

---
+++ xmonad/.xmonad/lib/MprisListener.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Mpris
+
+import Control.Concurrent
+import DBus
+import DBus.Client
+import Data.IORef
+import System.IO
+
+mprisEventMatcher :: MatchRule
+mprisEventMatcher = matchAny
+  { matchSender = Nothing
+  , matchDestination = Nothing
+  , matchPath = Just "/org/mpris/MediaPlayer2"
+  , matchInterface = Just "org.freedesktop.DBus.Properties"
+  , matchMember = Just "PropertiesChanged"
+  }
+
+mprisCallBack :: IORef (Maybe Player) -> Client -> Signal -> IO ()
+mprisCallBack currentPlayer client s = do
+  let Just sender = signalSender s
+  let items = dictionaryItems $ unpack (signalBody s !! 1)
+  case lookup (toVariant ("PlaybackStatus" :: String)) items of
+    Just a ->
+      case (unpack . unpack) a :: String of
+        "Playing" -> getMprisPlayer client sender >>= writeIORef currentPlayer . Just
+        _ -> return ()
+    Nothing -> return ()
+
+findInitialActivePlayer :: IO (Maybe Player)
+findInitialActivePlayer =
+  withMprisPlayers $ \players ->
+    return $ case filter isPlaying players of
+      x:_ -> Just x
+      _   -> Nothing
+  where isPlaying (Player { status = status }) = status == Playing
+
+main :: IO ()
+main = do
+  currentPlayer <- newIORef Nothing
+  client <- connectSession
+  findInitialActivePlayer >>= writeIORef currentPlayer
+  addMatch client mprisEventMatcher (mprisCallBack currentPlayer client)
+  hSetBuffering stdout LineBuffering
+  loop currentPlayer client
+
+loop :: IORef (Maybe Player) -> Client -> IO ()
+loop currentPlayer client = do
+  threadDelay 800000
+  currentPlayer' <- readIORef currentPlayer
+  case currentPlayer' of
+   Just p -> do
+     p' <- getMprisPlayer client . Mpris.player $ p
+     putStrLn $ formatPlayerXmobar p'
+   Nothing -> return ()
+  loop currentPlayer client
