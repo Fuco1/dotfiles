8b3226dd9be8fd492db7f7615145d9d8607a1b73
Matus Goljer <matus.goljer@gmail.com>
2015-11-15 13:08:31 +0100

    Formatting

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,12 +4,14 @@ end
 
 function fish_prompt --description "Write out the prompt"
   set -l fish_prompt_cwd
+
   switch $USER
     case root
       set fish_prompt_cwd (set_color ff5f5f)
     case '*'
       set fish_prompt_cwd (set_color 5f5fff)
   end
+
   echo -n -s (set_color "$fish_color_autosuggestion") (date "+[%H:%M:%S]") (set_color 55ff55) "$USER" @ (hostname) (set_color normal) ":" "$fish_prompt_cwd" (get_pwd) (set_color normal) "
 > "
 end
