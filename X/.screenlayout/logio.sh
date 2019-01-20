8c8e3ff870c5fabbd84d5453fdd2a798f65d1dfa
Matus Goljer <goljer@logio.cz>
2019-01-20 10:12:37 +0100

    Update the logio screen setup

--- X/.screenlayout/logio.sh
+++ X/.screenlayout/logio.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --mode 1920x1200 --pos 1600x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --mode 1600x900 --pos 0x300 --rotate normal --output VGA1 --mode 1920x1200 --pos 3520x0 --rotate normal
+xrandr --output VGA-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
