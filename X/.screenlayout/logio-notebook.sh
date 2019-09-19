684a2a9a3ece091c39eaaf6820ee0e63d3313173
Matus Goljer <matus.goljer@gmail.com>
2019-09-19 15:49:09 +0200

    chore(X): delete obsolete scripts

--- X/.screenlayout/logio-notebook.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --mode 1920x1200 --pos 3648x0 --rotate left --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --mode 1600x900 --pos 2048x1020 --rotate normal --output VGA1 --mode 2048x1152 --pos 0x768 --rotate normal
